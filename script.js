af32db55732528c36f1f627fff436a7432a69666
Ankit choudhary <ankitjat3232@gmail.com>
2026-04-15 14:55:12 -0400

    my first commit

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 'use strict';
-//aaa
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Numberrr ';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+console.log(document.querySelector('.guess').valuealue);
+document.querySelector('.guess').value = 23;
