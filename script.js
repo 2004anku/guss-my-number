2920d5fce94526a2b2c739b498a08d33cc04280f
Ankit choudhary <ankitjat3232@gmail.com>
2026-04-23 17:36:03 -0400

    update to new

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-'use strict';
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Numberrr ';
+// 'use strict';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Numberrr ';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-console.log(document.querySelector('.guess').valuealue);
-document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').valuealue);
+// document.querySelector('.guess').value = 23;
 // Is this in git hub
 
 // quize app
+/*
 const question = new Map([
   ['question', 'What is the programming language in the world ?'],
   [1, 'C'],
@@ -28,3 +29,20 @@ for (const [key, value] of question) {
 const answer = Number(prompt('Your answer'));
 console.log(answer);
 console.log(question.get(question.get('correct') === answer));
+*/
+
+//funcation to book seat in flight
+
+const lufthansa = {
+  airline: 'lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightnum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightnum}`,
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightnum},name` });
+  },
+};
+lufthansa.book(36, 'Ankit');
+lufthansa.book(37, 'Ashish');
