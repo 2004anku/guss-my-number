686f3a3ed7fc0bfc8df6613c6b348b486be449c6
Ankit choudhary <ankitjat3232@gmail.com>
2026-05-01 14:15:30 -0400

    Todays Learnign

--- script.js
+++ script.js
@@ -372,3 +372,141 @@ console.log(y);
 
 const z = Array.from({ length: 7 }, (cur, i) => i + 1);
 console.log(z);*/
+/////////////////////////////////////////////////////////////
+// LECTURES
+/*
+console.log(23 === 23.0);
+
+// Base 10-0to 9
+// Binary nase 2- 0,1
+console.log(0.1 + 0.2 === 0.3);
+// convert number
+console.log(Number('23'));
+console.log(+'23');
+
+// Parsing
+console.log(Number.parseInt('30px'));
+console.log(Number.parseInt('e30'));
+console.log(Number.parseFloat('54.455remm'));
+
+// Check if value is NaN
+console.log(Number.isNaN(20));
+console.log(Number.isNaN('20'));
+console.log(Number.isNaN(+'20X'));
+console.log(Number.isNaN(23 / 0));
+
+// CHecking if vlue is numner
+console.log(Number.isFinite(20));
+console.log(Number.isFinite('223'));
+console.log(Number.isFinite(+'23jf'));
+console.log(Number.isFinite(23 / 0));
+
+console.log(Number.isInteger(23));
+console.log(Number.isInteger(23.0));
+console.log(Number.isInteger(23 / 0));
+*/
+// Math and Rounding
+/*
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
+
+console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, '23', 11, 2));
+console.log(Math.max(5, 18, '23px', 11, 2));
+console.log(Math.min(5, 18, '23', 11, 2));
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+console.log(Math.trunc(Math.random() * 6 + 1));
+// to get some random nummber
+
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1));
+console.log(randomInt(10, 20));
+
+// Rounding Integer(USE TO REMOVE DECIMAL VALUES )
+console.log(Math.trunc(23.3543));
+
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
+
+console.log(Math.floor(23.3));
+console.log(Math.floor(23.9));
+
+// Runding decimals
+console.log((2.7).toFixed(0));
+console.log((2.7).toFixed(3));
+console.log((2.345).toFixed(2));
+console.log(+(2.345).toFixed(2));
+*/
+/*
+// The Remainder Operator
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    // 0, 2, 4, 6
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    // 0, 3, 6, 9
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
+*/
+/*
+// Numaric sepration
+// 287,460,000,000
+
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.1415;
+console.log(PI);
+
+console.log(Number('2300000'));
+*/
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(2 ** 53 + 1);
+console.log(2 ** 53 + 2);
+console.log(2 ** 53 + 3);
+console.log(2 ** 53 + 4);
+
+console.log(12345678234234398548357398457893478943765432n);
+console.log(BigInt(1234567823423439854835739845789347894376543));
+
+const huge = 2312312342354353654634563456345645n;
+const num = 23;
+console.log(huge * BigInt(num));
+
+console.log(20n > 15);
+console.log(20n === 20);
+console.log(typeof 20n);
+console.log(20n == '20');
+
+console.log(huge + ' is really big!!!!!!!');
+
+// DIVISION
+console.log(11n / 3n);
+console.log(10 / 3);
